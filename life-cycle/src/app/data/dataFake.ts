e438c188606abd2eb84c185e9a3999db132e7fd3
Ryan <silvallucena02@email.com>
2024-02-25 18:07:58 -0300

    Arquivo

---
+++ life-cycle/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Lewis Hamilton será piloto da Ferrari em 2025",
+    "description": "Um dos maiores pilotos de todos os tempos, o inglês heptacampeão mundial vai trocar a Mercedes pela Ferrari em 2025. Hamilton vai substituir o espanhol Carlos Sainz.",
+    "photoCover":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Lewis_Hamilton_2022_S%C3%A3o_Paulo_Grand_Prix_%2852498120773%29_%28cropped%29.jpg/640px-Lewis_Hamilton_2022_S%C3%A3o_Paulo_Grand_Prix_%2852498120773%29_%28cropped%29.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Pré-temporada F1 2024: Leclerc lidera último dia de testes",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
